5c16ebe8a7b48149ac642c570d83bfddecfcd8cf
PolkaMC <114118832+PolkaMC@users.noreply.github.com>
2026-09-18 23:29:39 -0500

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,29 @@
+window.addEventListener('click', function(event){
+
+    if(event.target.hasAttribute('data-action')) {
+
+        const counterWrapper = event.target.closest('.counter-wrapper');
+
+        const counter = counterWrapper.querySelector('[data-counter');
+
+        if(event.target.dataset.action === "plus") {
+            counter.innerText = ++counter.innerText;
+
+            if(event.target.closest('.cart-wrapper')) {
+                toggleCartStatus();
+            }
+        } else if (event.target.dataset.action === 'minus') {
+
+            if(event.target.closest('.cart-wrapper')) {
+                if(parseInt(counter.innerText) > 0) {
+                    counter.innerText = --counter.innerText;
+                }
+                else {
+                    event.target.closest('.cart-item').remove();
+                }
+                toggleCartStatus();
+            }
+        }
+    }
+
+})
